a379162bee5271c8a7a2395c587d8777bd1c319c
devaajanne <devaajanne@gmail.com>
2024-11-12 14:46:41 +0200

    Added React Router navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import "./stylings/App.css";
+import { Link, Outlet } from "react-router-dom";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <nav>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/customerlist"}>Customers</Link>
+        <Link to={"/traininglist"}>Trainings</Link>
+        <Link to={"/trainingcalendar"}>Calendar</Link>
+        <Link to={"/trainingstatistics"}>Statistics</Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,49 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import "./Stylings/index.css";
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import Home from "./components/Home.jsx";
+import CustomerList from "./components/CustomerList.jsx";
+import TrainingList from "./components/TrainingList.jsx";
+import TrainingCalendar from "./components/TrainingCalendar.jsx";
+import TrainingStatistics from "./components/TrainingStatistics.jsx";
+import Error from "./components/Error.jsx";
+
+import "./Stylings/index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        element: <Home />,
+        index: true,
+      },
+      {
+        path: "/customerlist",
+        element: <CustomerList />,
+      },
+      {
+        path: "/traininglist",
+        element: <TrainingList />,
+      },
+      {
+        path: "/trainingcalendar",
+        element: <TrainingCalendar />,
+      },
+      {
+        path: "/trainingstatistics",
+        element: <TrainingStatistics />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
