2310ad48a67a583e2174ddcc5f50c5da3bb37245
devaajanne <devaajanne@gmail.com>
2024-11-14 11:44:38 +0200

    Added customer list to Customers component

--- src/components/Customers.jsx
+++ src/components/Customers.jsx
@@ -1,7 +1,58 @@
+import { AgGridReact } from "ag-grid-react";
+import { useState, useEffect } from "react";
+import { fetchCustomers} from "../utils/api_requests";
+
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
+
 export default function Customers() {
+  const [customers, setCustomers] = useState([]);
+  const [columnDefs, setColumnDefs] = useState([
+    {
+      headerName: "First name",
+      field: "firstname",
+      filter: true,
+      floatingFilter: true,
+    },
+    {
+      headerName: "Last name",
+      field: "lastname",
+      filter: true,
+      floatingFilter: true,
+    },
+    {
+      headerName: "Street address",
+      field: "streetaddress",
+      filter: true,
+      floatingFilter: true,
+    },
+    {
+      headerName: "Post code",
+      field: "postcode",
+      filter: true,
+      floatingFilter: true,
+    },
+    { headerName: "City", field: "city", filter: true, floatingFilter: true },
+    { headerName: "Email", field: "email", filter: true, floatingFilter: true },
+    { headerName: "Phone", field: "phone", filter: true, floatingFilter: true },
+  ]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const customerData = await fetchCustomers();
+      setCustomers(customerData);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <p>Customers</p>
+      <div className='ag-theme-material' style={{ width: 1500, height: 1500 }}>
+        <AgGridReact
+          rowData={customers}
+          columnDefs={columnDefs}
+        />
+      </div>
     </div>
   );
 }
