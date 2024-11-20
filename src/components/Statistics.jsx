709d687ccfa501d339b3a9ae00730b42f1daad3d
devaajanne <devaajanne@gmail.com>
2024-11-20 11:50:53 +0200

    Added bar graph to Statistics component

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,7 +1,56 @@
+import { useState, useEffect } from "react";
+import { fetchTrainings } from "../utils/api_requests";
+import _ from "lodash";
+import {
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Bar,
+  Label,
+  LabelList,
+} from "recharts";
+
 export default function Statistics() {
+  const [trainings, setTrainings] = useState([]);
+  const [data, setData] = useState([]);
+
+  const fetchTrainingData = async () => {
+    const trainingsData = await fetchTrainings();
+    setTrainings(trainingsData._embedded.trainings);
+  };
+
+  useEffect(() => {
+    fetchTrainingData();
+  }, []);
+
+  useEffect(() => {
+    if (trainings.length > 0) {
+      const dataGrouped = _.groupBy(trainings, "activity");
+      console.log(dataGrouped);
+      const dataSummed = _.map(dataGrouped, (activityList, activityname) => ({
+        activity: activityname,
+        totalDuration: _.sumBy(activityList, "duration"),
+      }));
+      console.log(dataSummed);
+      setData(dataSummed);
+    }
+  }, [trainings]);
+
   return (
     <div>
-      <p>Statistics</p>
+      <BarChart width={1000} height={500} data={data}>
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='activity'></XAxis>
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey='totalDuration' fill='#2196f3'>
+          <LabelList dataKey='totalDuration' position='top' />
+        </Bar>
+      </BarChart>
     </div>
   );
 }
